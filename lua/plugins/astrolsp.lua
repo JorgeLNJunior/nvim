ffb1e9dbba91d056d50fb5aa3c024a0d538db3b4
Jorge Junior <jorgelnjunior@gmail.com>
2025-03-27 12:56:00 -0300

    chore: disable ts_server formatting

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -30,6 +30,8 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
+        "volar",
+        "ts_ls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
@@ -43,9 +45,7 @@ return {
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      htmx = { filetypes = { "html", "templ" } },
       html = { filetypes = { "html", "templ" } },
-      volar = { filetypes = { "typescript", "javascript", "vue" } },
       ts_ls = {
         init_options = {
           plugins = {
